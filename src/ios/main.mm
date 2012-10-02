59890d4eae57c3847e2d0df4463ca01acf06d80a
danomatika <danomatika@gmail.com>
2012-10-01 23:57:16 -0400

    updated ios xcode project, dosen't build yet ...

--- src/ios/main.mm
+++ src/ios/main.mm
@@ -16,7 +16,7 @@
 //========================================================================
 int main() {
 
-	ofSetupOpenGL(1024, 768, OF_FULLSCREEN);			// <-------- setup the GL context
+	ofSetupOpenGL(1024, 768, OF_FULLSCREEN);
 
 	ofRunApp(new App);
 	//ofRunAppWithAppUtils(new App);
